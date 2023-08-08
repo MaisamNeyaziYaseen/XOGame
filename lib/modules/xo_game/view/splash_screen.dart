6fc47138f0471131badf874edc2a74506ec6fe27
Ahmed Khamis <khames24@gmail.com>
2023-08-08 11:53:06 +0300

    first

---
+++ lib/modules/xo_game/view/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/animation_builder/play_animation_builder.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: PlayAnimationBuilder<double>(
+          duration: const Duration(seconds: 2),
+          curve: Curves.easeInOut,
+          tween: Tween<double>(begin: -100.0, end: 200.0),
+          builder: (context, value, child) {
+            return Image.asset( "logo.png",width: value, height: value);
+          },
+          onCompleted: () {
+            // Navigator.pushReplacement(
+            //     context,
+            //     MaterialPageRoute(
+            //         builder: (context) => const HomeScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
